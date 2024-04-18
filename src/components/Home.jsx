63a00c9a32162cf20c73fa407c14eab675336caa
yanitbee <yanithaftomm@gmail.com>
2024-04-18 06:41:13 -0700

    uhhhhhh

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,6 +8,13 @@ const Home=()=> {
   const togglePopup =()=>{
     setPopup(!popup)
   }
+
+  if(popup){
+    document.body.classList.add('active-popup')
+  } else{
+    document.body.classList.remove('active-popup')
+  }
+
   return (
     <div id='main'>
 
@@ -18,17 +25,30 @@ const Home=()=> {
       <div className='header-btns'>
         <button className='header-btn' onClick={togglePopup}>Register Now</button>
 
+      {popup && (
         <div className="popup">
-          <div className="overlay">
-            <div className="popup-content">
-              <h2>Register</h2>
-              <p>qwertyulkjhc jkhgasdyu</p>
-              <button className='header-btn' onClick={togglePopup}>close</button>
-            </div>
+        <div onClick={togglePopup} className="overlay"></div>
+          <div className="popup-content">
+            <h3>Register</h3>
+            <input type="radio" name="user" value="freelancer" /> Freelancer
+            <input type="radio" name="user" value="employer" /> Employer <br />
+            <input className="input" type="text" placeholder='Fullname' />
+            <input className="input" type="text" placeholder='Phonenumber' />
+            <input className="input" type="email" placeholder='Email' /> 
+            <input className="input" type="password" placeholder='Password' /> <br />
+            <input type="radio" name="gender" value="male" /> Male
+            <input type="radio" name="gender" value="female" /> Female
+            <br /> <br />
+            <button className='popup-btn' >Submit</button>
+            <p>Already have an account. LogIn</p>
+            <button className='popup-btn' onClick={togglePopup}>Close</button>
           </div>
-        </div>
+        
       </div>
-  </div>
+
+      )}
+    </div>
+</div>
     </div>
 
   )
